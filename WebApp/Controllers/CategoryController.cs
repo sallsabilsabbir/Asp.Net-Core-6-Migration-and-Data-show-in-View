833b7242ff2f2d43a61d2bdc35d977a3f6002623
scube <scube@SCUBE>
2022-06-06 17:02:07 +0600

    Add project files.

---
+++ WebApp/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Data;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db=db; 
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category>objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ WebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
